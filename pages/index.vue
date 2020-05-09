d45709231764e2386cfdd2c41af50efeb027124f
Zack <warrior.killz@gmail.com>
2020-05-09 10:52:13 +0800

    Initial Nuxt App Created

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <h1>This is index.vue</h1>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto bg-teal-200 min-w-full;
+}
+</style>
